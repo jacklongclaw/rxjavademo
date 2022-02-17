8c712c56f649dbf82bbcd2a40e9c5f2e8cf7bca9
liujiajun <liujiajun@growingio.com>
2022-02-17 17:35:47 +0800

    add test

--- src/test/java/com/jack/demo/TestCompletable.java
+++ src/test/java/com/jack/demo/TestCompletable.java
@@ -1,6 +1,7 @@
 package com.jack.demo;
 
 import io.reactivex.Completable;
+import io.reactivex.Maybe;
 import io.reactivex.Observable;
 import org.junit.jupiter.api.Test;
 
@@ -10,5 +11,13 @@ public class TestCompletable {
     public void test(){
         Completable.create(emitter ->  emitter.onComplete()).andThen(Observable.range(0,10))
         .subscribe(intValue -> System.out.println(intValue));
+    }
+
+    @Test
+    public void testMaybe(){
+        Maybe.create(emitter -> {
+            emitter.onSuccess("hello world");
+            emitter.onSuccess("hello kitty");
+        }).subscribe(s-> System.out.println("---------------------"+s));
     }
 }
